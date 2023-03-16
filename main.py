b6799b4c1ca94c3724f1bde7c10c7d830005f145
Andy Nichols <AndyNichols@pacbell.net>
2023-03-15 23:08:36 -0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #user = input("Input fencer USAF number, first name, and last name separated by spaces\n\n")
-user = "100146376 sophie guan"
+user = "100124844 andy nichols"
 
 
 userData = user.split()
@@ -19,5 +19,9 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find_all(class_="table-group-divider")
 for result in results:
-    if (result.select_one('td:nth-child(1)').get_text() == 'Foil') & (result.select_one('td:nth-child(2)').get_text() == 'DE'):
-        print(result.select_one('td:nth-child(3)').get_text())
+    try:
+        if (result.select_one('td:nth-child(1)').get_text() == 'Foil') & (result.select_one('td:nth-child(2)').get_text() == 'DE'):
+            rating = result.select_one('td:nth-child(3)').get_text()
+            print('\n' + rating + '\n')
+    except:
+        print('\nSomething went wrong\n')
